7b56d5ca91cac387560dd7f699fa8f2f4ee30990
Priyanka <sumit>
2022-12-16 17:14:48 +0530

    change in code

--- src/Neostox/NeostoxBase.java
+++ src/Neostox/NeostoxBase.java
@@ -7,11 +7,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 
 import Upstoxutility.UtilityNew;
+ 
+
 
 public class NeostoxBase {
 protected static WebDriver driver;
  public void LaunchBrowser() throws IOException
 {
+	 
+	 
+	 
+	 
 	System.setProperty("webdriver.chrome.driver","C:\\9jully\\selenium\\chromedriver.exe");
 driver =new ChromeDriver();
 driver.get(UtilityNew.readDatafromPropertyFile("devurl"));
